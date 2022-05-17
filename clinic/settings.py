1671b8a5ac96bcf95a4aa1cd9043ce596155560c
Deepak patil <deepak1786patil@gmail.com>
2022-05-17 17:29:55 +0530

    Update settings.py

--- clinic/settings.py
+++ clinic/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/deepakpatil/clinic/static/'
+STATIC_ROOT = '/home/deepakpatil/clinic/clinic/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
